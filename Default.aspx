1fdc547d8f1fb1f24527cca839e55a59c494ac9e
Tolulope <tolesy2003@outlook.com>
2018-03-06 16:04:25 +0200

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeFirstPapaTolusWebsite.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="height: 729px">
+    
+        <asp:Image ID="resultImage" runat="server" style="margin-right: 4px" Width="168px" />
+        <asp:Label ID="logoNameLabel" runat="server" Font-Bold="True" Font-Size="Larger" Text="Papa Tolu's Pizza and Software"></asp:Label>
+        <br />
+        <br />
+        <asp:RadioButton ID="babyToluRadioButton" runat="server" Text="Baby Tolu Size(10&quot;)-$10" GroupName="SizeGroup" />
+        <br />
+        <asp:RadioButton ID="MamaToluRadioButton" runat="server" Text="Mama Tolu Size(13&quot;)-$13" GroupName="SizeGroup" />
+        <br />
+        <asp:RadioButton ID="PapaToluRadioButton" runat="server" Text="Papa Tolu Size(16&quot;)-$16" GroupName="SizeGroup" />
+        <br />
+        <br />
+        <asp:RadioButton ID="thinCrustRadioButton" runat="server" Text="Thin Crust" />
+        <br />
+        <asp:RadioButton ID="deepDishRadioButton" runat="server" Text="Deep Dish(+$2)" />
+        <br />
+        <br />
+        <asp:CheckBox ID="pepperoniCheckBox" runat="server" Text="Pepperoni (+$1.50)" />
+        <br />
+        <asp:CheckBox ID="onionsCheckBox" runat="server" Text="Onions(+$0.75)" />
+        <br />
+        <asp:CheckBox ID="greenPeppersCheckBox" runat="server" Text="Green Peppers(+$0.50)" />
+        <br />
+        <asp:CheckBox ID="redPeppersCheckBox" runat="server" Text="Red Peppers(+$0.75)" />
+        <br />
+        <asp:CheckBox ID="anchoviesCheckBox" runat="server" Text="Anchovies(+$2.00)" />
+        <br />
+        <br />
+        Papa Tolu&#39;s&nbsp; <asp:Label ID="specialDealLabel" runat="server" ForeColor="Red"></asp:Label>
+        <br />
+        <br />
+        Save $2.00 when you add Pepperoni, Green Peppers and Anchovies Or Pepperoni , Red Peppers and Onions to your pizza.<br />
+        <br />
+        <asp:Button ID="purchaseButton" runat="server" OnClick="purchaseButton_Click" Text="Purchase" />
+        <br />
+        <br />
+        <br />
+        <asp:Label runat="server" Text="Total:"></asp:Label>
+&nbsp;<asp:Label ID="totalLabel" runat="server">$0.00</asp:Label>
+        <br />
+        <br />
+        Sorry , at this time you can only order one pizza online, and pick-up only... we need a better website!<br />
+        <br />
+        <br />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeFirstPapaTolusWebsite
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            resultImage.ImageUrl = "PapaTolu.png";
+            specialDealLabel.Text = "Special Deal";
+        }
+
+        protected void purchaseButton_Click(object sender, EventArgs e)
+        {
+            // Declared a double variable named total
+            double total = 0.0;
+
+        // Checked with if() conditional statement if the user selected one out of the 3 radio buttons
+        // On the default.aspx I set the radio button group name to SizeGroup, this gives access to only a selection
+            if (babyToluRadioButton.Checked)
+            {
+                total = 10.0;
+            }
+            else if (MamaToluRadioButton.Checked)
+                {
+                total = 13.0;
+                }
+            else if (PapaToluRadioButton.Checked)
+            {
+                total = 16.0;
+            }
+            else
+            {
+                total = 0.00;
+            }
+            if (deepDishRadioButton.Checked)
+            {
+                total += 2.0; //total = total + 2.0;
+            }
+            /* Ternary operation instead of If() condition
+             1 Identify the variable that will be modified (total in this case)
+             2 Inside the parentheses, declare the condition to be evaluated (in this case, pepperoniCheckBox checked?)
+             3 If so, the operation after the question mark will be executed (total + 1.5)
+             4 Otherwise, if the condition is not true, return the value after the colon (total)*/
+            total = (pepperoniCheckBox.Checked) ? total + 1.5 : total;
+            total = (onionsCheckBox.Checked) ? total + 0.75 : total;
+            total = (greenPeppersCheckBox.Checked) ? total + 0.5 : total;
+            total = (redPeppersCheckBox.Checked) ? total + 0.75 : total;
+            total = (anchoviesCheckBox.Checked) ? total + 2.0 : total;
+            // checked if either of these conditions inside the if statement is met and if so subtract 2 from our total
+            if ((pepperoniCheckBox.Checked && greenPeppersCheckBox.Checked && anchoviesCheckBox.Checked) || (pepperoniCheckBox.Checked && redPeppersCheckBox.Checked && onionsCheckBox.Checked))
+            {
+                total -= 2.0;
+            }
+            // output the final result.
+            totalLabel.Text = "$" + total.ToString();
+        }
+    }
+}
+
+
